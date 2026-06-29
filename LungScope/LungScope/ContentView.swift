a0b0cda7a86355b594a6583ec8413e1757ff40cb
Silvio Stoykov <s.stoykov@sap.com>
2026-06-29 20:41:21 +0300

    Add audio logic

---
+++ LungScope/LungScope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LungScope
+//
+//  Created by Stoykov, Silvio on 29.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LungScope/LungScope/LungScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LungScopeApp.swift
+//  LungScope
+//
+//  Created by Stoykov, Silvio on 29.06.26.
+//
+
+import SwiftUI
+
+@main
+struct LungScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
